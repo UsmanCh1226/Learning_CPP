74d3ab060e98a5187532297889f3df91b6e7419d
UsmanCh1226 <usmanch@umich.edu>
2025-12-29 17:09:43 -0500

    Day 1 Session 1

---
+++ helloworld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+
+    // this is a comment!
+    /*
+    this is a 
+    multi-line
+    comment!
+    */
+   
+    std::cout << "I like pizza!" << '\n';
+    std::cout << "It's really good!" << '\n';
+    return 0;
+}
