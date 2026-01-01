0ad699cb13d2e8c7ac522d232e38cffcf87bfee2
UsmanCh1226 <usmanch@umich.edu>
2026-01-01 13:00:42 -0500

    Day 2 Session 1

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,19 +1,37 @@
-<<<<<<< HEAD
 #include <iostream>
 
 int main(){
+    int age = 20;
+    int year = 2026;
+    int days = 7;
 
-    // this is a comment!
-    /*
-    this is a 
-    multi-line
-    comment!
-    */
-   
-    std::cout << "I like pizza!" << '\n';
-    std::cout << "It's really good!" << '\n';
+    double price = 10.99;
+    double gpa = 2.5;
+    double temperature = 25.1;
+
+    char grade = 'A';
+    char initial = 'U';
+    char currency = '$';
+
+    bool student = true;
+    bool power = true;
+    bool forsale = false;
+
+    std::string name = "Usman";
+    std::string day = "Thursday";
+    std::string food = "Butter Chicken";
+    std::string address = "123 Fake St.";
+    std::cout << name; 
+
+
+
+
+    
+
+
+
+
     return 0;
 }
-=======
 
->>>>>>> d4f174fbd3313a815dcc420309434163734d9863
+
