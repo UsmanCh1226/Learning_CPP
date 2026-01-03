c7e40400ce15f0c94e4431a1117acdc998bb3240
UsmanCh1226 <usmanch@umich.edu>
2026-01-02 19:43:37 -0500

    Hypotenuse calculator

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,22 +7,20 @@
 
 int main(){
 
-    double x = 3.14;
-    double y = 4;
-    double z;
+    double a;
+    double b;
+    double c;
 
-    //z = std::max(x,y);
-    //z = std::min(x,y);
-    //z = pow(2,3);
-    //z = sqrt(3);
-    //z = abs(-3);
-    //z = round(x);
-    //z = ceil(x);
-    z = floor(x);
+    std::cout << "Enter side A: ";
+    std::cin >> a;
+
+    std::cout << "Enter side B: ";
+    std::cin >> b;
+
     
+    c = sqrt(pow(a, 2) + pow(b, 2));
 
-    std::cout << z;
-
+    std::cout << "side C: " << c << std::endl;
 
     return 0;
 }
