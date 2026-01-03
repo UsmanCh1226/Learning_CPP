c363006e549e7b3f828ee7d6efa9dd77f5689728
UsmanCh1226 <usmanch@umich.edu>
2026-01-03 18:32:31 -0500

    Temperature Conversion Program

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,22 +2,34 @@
 
 int main() 
 {
+    double temp;
+    char unit;
+    
+    std::cout << "****** Temperature Conversion ******\n";
+    std::cout << "F = Fahrenheit\n";
+    std::cout << "C = Celsius\n";
+    std::cout << "What unit would you like to convert to? ";
+    std::cin >> unit;
+    
+    if(unit == 'F' || unit == 'f'){
+        std::cout << "Enter the temperature in Celsius: ";
+        std::cin >> temp;
 
-    //int grade = 50;
+        temp = (1.8 * temp) + 32;
+        std::cout << "The temperature in Fahrenheit is: " << temp << " F\n";
+    } 
+    else if(unit == 'C' || unit == 'c'){
+        std::cout << "Enter the temperature in Fahrenheit: ";
+        std::cin >> temp;
 
-    //grade >= 60 ? std::cout << "You pass!" : std::cout << "You fail.";
+        temp = (temp - 32) / 1.8;
+        std::cout << "The temperature in Celsius is: " << temp << " C\n";
+    }
+    else {
+        std::cout << "Invalid unit selection. Please choose 'F' or 'C'.\n";
+    }
 
-    //int number = 9;
-    //number % 2   ? std::cout << "Odd" : std::cout << "Even"; 
-
-    bool hungry = true;
-    //hungry ? std::cout << "You are hungry" : std::cout << "You are full"
-    std::cout << (hungry ? "You are hungry" : "You are full");
+    std::cout << "******************\n";
 
     return 0;
 }
-
-    
-
-
- 
