6b2c493c7f910446f18fb575c07f34c09ad0baef
UsmanCh1226 <usmanch@umich.edu>
2026-01-04 17:44:52 -0500

    While Loops

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,26 +4,14 @@ int main()
 {
     std::string name;
 
-    std::cout << "Enter your name: ";
-    std::getline(std::cin, name);
+    while(1==1) {
+        std::cout << "HELP! I'M STUCK IN A INFINITE LOOP ";
+        std::getline(std::cin, name);
+    }
 
-    //if(name.length() > 12){
-      //  std::cout << "Your name can't be over 12 characters.";
-    
-    //}
-    //else{
-      //  std::cout << "Welcome " << name << "!";
-    //}
-    //name.append("@gmail.com");
-    
-    //std::cout << "Your username is now " << name;
+    std::cout << "Hello " << name;
 
-    
- 
-    //name.insert(0,"@");
-    name.erase(0, 5); 
 
-    std::cout << name;
 
     return 0;
 }
