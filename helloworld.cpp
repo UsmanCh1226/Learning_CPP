7c8b4b1a1b018072ae6e2eecf61234ce78b826a9
UsmanCh1226 <usmanch@umich.edu>
2026-01-05 10:54:53 -0500

    random number generator

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,37 +1,19 @@
 #include <iostream>
+#include <ctime>
 
 int main() 
- {
-    int rows;
-    int columns;
-    char symbol;
+{
+    srand(time(NULL));
 
-    std::cout << "How many rows: ";
-    std::cin >> rows;
+    int num1 = (rand() % 6) + 1;
+    int num2 = (rand() % 6) + 1;
+    int num3 = (rand() % 6) + 1;
 
-    std::cout << "How many columns: ";
-    std::cin >> columns;
-
-    std::cout << "Enter a symbol to use: ";
-    std::cin >> symbol; 
+    std::cout << num1 << '\n';
+    std::cout << num2 << '\n';
+    std::cout << num3 << '\n';
 
     
-     
-
-    for(int i = 1; i <= rows; i++){
-        for(int j = 1 ; j <= columns; j++){
-            std::cout << symbol << ' ';
-        }  
-        std::cout << '\n';
- 
-    }
-
-    
-
-    
-    
- 
-
-
     return 0;
 }
+
